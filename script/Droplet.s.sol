30290b47b7f1e421f4afbefdee72136ffda3eaf9
Zk <zklim95@hotmail.com>
2025-02-22 14:20:37 -0700

    update script

--- script/Droplet.s.sol
+++ script/Droplet.s.sol
@@ -16,10 +16,21 @@ contract DropletScript is Script {
     }
 
     function runGrantRoles() public {
+        vm.createSelectFork("base");
+        vm.activeFork();
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
-
-        address token = address(0xABCD);
+        address token = address(0xd8a698486782d0d3fa336C0F8dd7856196C97616);
         address admin = address(0x1234);
+
+        droplet = ERC20StorageVersioned(token);
+        droplet.grantRole(droplet.TOKEN_MANAGER_ROLE(), admin);
+
+        vm.stopBroadcast();
+        vm.createSelectFork("base_testnet");
+        vm.activeFork();
+        vm.startBroadcast(vm.envUint("PRIVATE_KEY_TESTNET"));
+        token = address(0xc9e3A0b2d65cbb151Fa149608f99791543290d6d);
+        admin = address(0x1234);
 
         droplet = ERC20StorageVersioned(token);
         droplet.grantRole(droplet.TOKEN_MANAGER_ROLE(), admin);
